6bab0ba8639eb51b8accd0ae9c4fca0f90b09808
Albert Einstein <javastraat@hotmail.com>
2024-09-06 13:29:37 +0200

    initial release of json data

---
+++ getdata.sh
@@ -0,0 +1,5 @@
+wget -O hamvoip.json http://192.168.2.111:8181/hamvoip/all
+wget -O dapnet.json http://192.168.2.111:8181/dapnet/all
+wget -O rptrs.json http://192.168.2.111:8181/rptrs/all
+wget -O radioid.json http://192.168.2.111:8181/radioid/all
+
